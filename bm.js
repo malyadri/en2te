4d70dbf3aa54d40d941947355c25f5522bd538fb
Vamsee Kanakala <vkanakala@gmail.com>
2010-07-24 16:31:09 +0530

    modifying readme and adding some code

--- bm.js
+++ bm.js
@@ -1,9 +1,28 @@
+function toQueryString(obj) {    
+  var parts = [];    
+  for(var each in obj) if (obj.hasOwnProperty(each)) {
+    parts.push(encodeURIComponent(each) + '=' + encodeURIComponent(obj[each]));    
+  }    
+  return parts.join('&');  
+};
+
+function res_handler(rsp) {   
+    if(rsp.data){           
+        yql_results = rsp.data;
+    }
+}
+
 document.body.ondblclick = function() {
     var sel = window.getSelection();
+    var sel_txt = "";
+    var yql_base_uri = "http://query.yahooapis.com/v1/yql";
+    var yql_results = "";
+    var yql_query = "SELECT * from html where url = 'http://dictionary.reference.com/browse/weird' and xpath='//div[@dndata]'";
+
     if (sel.anchorNode.nodeType == Node.TEXT_NODE
         && sel.focusNode.nodeType == Node.TEXT_NODE) {
-        alert(sel.toString()); 
+        sel_txt = sel.toString(); 
     }
 };
 
-alert('Activated en2te!');
+alert('Activated en2te. Double click on a word to pull up meaning in Telugu ');
